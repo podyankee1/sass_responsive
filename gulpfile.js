2d396f089d21773db23182950145072589ad9d96
podyankee <podyankee@gmail.com>
2016-03-20 13:30:55 +0200

    js task completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var jade          = require('gulp-jade');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var gulpif = require('gulp-if');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var  imagemin = require('gulp-imagemin');
-var pngcrush = require('imagemin-pngcrush');
-var bourbon = require('node-bourbon');
+var gulp        = require('gulp'),
+ browserSync = require('browser-sync'),
+ sass        = require('gulp-sass'),
+ prefix      = require('gulp-autoprefixer'),
+ cp          = require('child_process'),
+ jade          = require('gulp-jade'),
+ concat = require('gulp-concat'),
+ uglify = require('gulp-uglify'),
+ gulpif = require('gulp-if'),
+ rename = require('gulp-rename'),
+ sourcemaps = require('gulp-sourcemaps'),
+ plumber = require('gulp-plumber'),
+ notify = require('gulp-notify'),
+  imagemin = require('gulp-imagemin'),
+ pngcrush = require('imagemin-pngcrush'),
+ bourbon = require('node-bourbon');
 bourbon.includePaths
 
 
 var env,
-	sassStyle;
+	  sassStyle,
+    sassSources,
+    jadeSources,
+    imgSources,
+    jsSources;
 
 env = process.env.NODE_ENV || 'development';
+sassSources = ['assets/css/'];
+jadeSources = ['_jadefiles/*.jade'];
+imgSources = ['assets/img/**/*.*'];
+jsSources  = ['assets/libs/jquery/dist/jquery.min.js','assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js']
 
-if (env==='development')
+if (env==='production')
 	{
 	sassStyle = 'expanded';
 } else {
@@ -63,7 +71,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 });
 
 
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+gulp.task('browser-sync', [ 'sass', 'jekyll-build'], function() {
 	browserSync({
 		server: {
 			baseDir: 'dist'
@@ -72,18 +80,24 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 	});
 });
 
+gulp.task('libs_js', function() {
+	return gulp.src(jsSources)
+		.pipe(customPlumber('Error Running JS'))
+		.pipe(concat('libs.min.js'))
+	  .pipe(gulp.dest('assets/js'))
+});
+
 gulp.task('js', function() {
-	return gulp.src(['assets/js/functions.js'])
+	return gulp.src(['assets/js/libs.min.js', 'assets/js/functions.js'])
 		.pipe(customPlumber('Error Running JS'))
 		.pipe(gulpif(env === 'production', uglify()))
-	.pipe(gulp.dest('dist/assets/js'))
-		.pipe(browserSync.reload({stream:true}));
+	  .pipe(gulp.dest('dist/assets/js'))
+		.pipe(browserSync.reload({stream:true}))
 });
 
 
-
 gulp.task('jade', function  () {
-	return gulp.src('_jadefiles/*.jade')
+	return gulp.src(jadeSources)
 	.pipe(customPlumber('Error Running JADE'))
 	.pipe(jade({pretty: true}))
 	.pipe(gulp.dest('_includes'));
@@ -93,7 +107,7 @@ gulp.task('jade', function  () {
  * Compile files from _scss into both dist/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-	return gulp.src('assets/css/*.scss')
+	return gulp.src(sassSources+"*.scss")
 		.pipe(customPlumber('Error Running Sass'))
 		.pipe(sourcemaps.init())
 		.pipe(sass({
@@ -110,7 +124,7 @@ gulp.task('sass', function () {
 
 
 gulp.task('images', function() {
-	gulp.src('assets/img/**/*.*')
+	gulp.src(imgSources)
 	.pipe(gulpif(env === 'production', imagemin({
 		progressive: true,
 		svgoPlugins: [{ removeViewBox: false }]
@@ -127,10 +141,11 @@ gulp.task('images', function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-	gulp.watch('assets/js/*.js', ['js']);
-	gulp.watch('assets/css/**/*.sass', ['sass']);
+	gulp.watch(sassSources+'**/*.sass', ['sass']);
 	gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
 	gulp.watch(['_jadefiles/*.jade'], ['jade']);
+  gulp.watch(jsSources, ['libs_js']);
+  gulp.watch(['assets/js/*.js'], ['js']);
 	gulp.watch(['assets/img/**/*.*'], ['images']);
 });
 
@@ -138,4 +153,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['js','sass','jekyll-rebuild','jade','images','browser-sync', 'watch']);
+  gulp.task('default', ['sass','jekyll-rebuild','jade','libs_js','js','images','browser-sync', 'watch']);
